366b3b845f375849837b12bfdfcdae09c133cea3
caioRafael <caiorafaelrg@gmail.com>
2021-11-13 18:07:40 -0300

    correcao da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://superheroapi.com/api/${process.env.REACT_APP_SEUPER_HERO_ACESS_TOKEN}/`,
-  headers: {
-    'Content-Type': '*/*',
-    'Access-Control-Allow-Origin': '*'
-  }
+  baseURL: `https://superheroapi.com/api.php/${process.env.REACT_APP_SEUPER_HERO_ACESS_TOKEN}/`,
 })
 
 export default api
