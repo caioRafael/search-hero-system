689c374390db58d48fe3dce539f04818a3c0daf9
caioRafael <caiorafaelrg@gmail.com>
2021-11-21 19:41:09 -0300

    powerstats styles

--- src/components/HeroPage/components/Card/index.tsx
+++ src/components/HeroPage/components/Card/index.tsx
@@ -1,4 +1,12 @@
 import { Hero } from "../../../../models/Hero"
+import {
+  GiHearts,
+  GiRockGolem,
+  GiBrain,
+  GiMightyForce,
+  GiSpeedometer,
+  GiBiceps
+} from "react-icons/gi"
 
 import './styles.scss'
 
@@ -14,6 +22,14 @@ export default function Card({ hero }: CardTypesProps) {
         <div>{hero.name}</div>
       </header>
       <img src={hero.image.url} alt="Hero" />
+      <footer className="power">
+        <span><GiHearts size={20} color="red" style={{ marginRight: 5 }} />{hero.powerstats.combat}</span>
+        <span><GiRockGolem size={20} color="gray" style={{ marginRight: 5 }} />{hero.powerstats.durability}</span>
+        <span><GiBrain size={20} color="pink" style={{ marginRight: 5 }} />{hero.powerstats.intelligence}</span>
+        <span><GiMightyForce size={20} color="green" style={{ marginRight: 5 }} />{hero.powerstats.power}</span>
+        <span><GiSpeedometer size={20} color="yellow" style={{ marginRight: 5 }} />{hero.powerstats.speed}</span>
+        <span><GiBiceps size={20} color="red" style={{ marginRight: 5 }} />{hero.powerstats.strength}</span>
+      </footer>
     </section>
   )
 }
